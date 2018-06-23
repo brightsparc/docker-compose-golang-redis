fb91114b16bd602a5c88c0d7bef1097a52c84769
Bright <julbrigh@8c8590a9d520.ant.amazon.com>
2018-06-23 21:24:43 +1000

    Updating app to include staged build and healthcheck

--- app.go
+++ app.go
@@ -1,27 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "gopkg.in/redis.v3"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-redis/redis"
 )
 
 func main() {
-    client := redis.NewClient(&redis.Options{
-        Addr: "db:6379",
-        Password: "",
-        DB: 0,
-    })
+	var addr = flag.String("db", "db:6379", "Redis host")
+	var host = flag.String("host", ":5000", "Redis host")
+	var ping = flag.Bool("ping", false, "Ping command")
+	flag.Parse()
 
-    pong, err := client.Ping().Result()
-    if err != nil {
-        panic(err)
-    }
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", pong)
-    })
+	client := redis.NewClient(&redis.Options{
+		Addr:     *addr,
+		Password: "",
+		DB:       0,
+	})
 
-    log.Fatal(http.ListenAndServe(":5000", nil))
+	if *ping {
+		fmt.Printf("Connecting to %s\n", *addr)
+		pong, err := client.Ping().Result()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(pong)
+		return
+	}
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		result, err := client.Incr("counter").Result()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "Hello, %d\n", result)
+	})
+
+	fmt.Printf("Listening on %s\n", *host)
+	log.Fatal(http.ListenAndServe(*host, nil))
 }
